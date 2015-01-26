4827098691fa9c91c3dad770a420ca0eb7d88757
Isaac Weinbach <isaacito@aol.com>
2015-01-26 13:20:53 -0500

    fisrt square created

---
+++ public/grid.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+
+$( ".square" ).mouseenter(function() { 
+    $( this ).css("background", "red");
+});
+
+$('.square').mouseleave(function(){
+        $(this).css("background", "black")
+
+
+} );
+
+
+
+  });
