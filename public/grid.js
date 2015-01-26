e1f63cef5428c2d35f7bd882705c39cc1d6ddc6c
Isaac Weinbach <isaacito@aol.com>
2015-01-26 16:48:33 -0500

    beast dante

--- public/grid.js
+++ public/grid.js
@@ -1,25 +1,48 @@
 
 $(document).ready(function(){
+var Grid = function(width, $container){
+    this.cells = [];
+    this.width = width;
+    this.$container = $container;
+    this.resizeContainer();
+};
 
+var Cell = function(x, y, node){
+
+};
+
+Grid.prototype.addCell = function(x, y, $node){
+    var cell = new Cell(x, y, $node);
+    this.cells.push(cell);
+    this.$container.append($node);
+};
+
+Grid.prototype.resizeContainer = function(){
+    this.$container.css("width", (this.width * 50).toString() + "px");
+    debugger
+};
+
+window.$grid = null;
 
 $('#start').click(function(){
-   var input =  prompt('Type an integer between 1 and 12');
+   var input =  prompt('Type an integer between 1 and 20');
+   var container = $('#grid_container');
    $('#grid_container').html(" ");
 
-if (input >= 1 && input <= 12 ){
-   
-   var square = $("#grid_container").width/input - 2
+if (input >= 1 && input <= 20 ){
+        $grid = new Grid(input, container);
 
         for (var i = 1; i <= input; i++){
             for (var j = 1; j <= input; j ++){
-                 $('#grid_container').append('<div class="grid_square"></div>');
+                    $grid.addCell(i, j, $('<div class="grid_square"></div>'));
+                 
+
+
             }
         
         $('#grid_container').append('<div class="new_row"></div>');
         }
-        
-        $('.grid_square').css('width',square);
-        $('.grid_square').css('height',square);
+
 
          $('.grid_square').mouseenter(function(){
             $(this).addClass('grid_square_lit');
@@ -32,40 +55,4 @@ if (input >= 1 && input <= 12 ){
    }
 });
 
-
-
 });
-
-
-
-// var start = function(option){
-
-//     $('#grid_container').html("");
-
-//     var input = prompt('Type an integer between 1 and 12');
-
-//     if (input >= 1 && input <= 12){ 
-
-//     var square_size = $('#grid_container').width()/input - 2; // The -2 accounts for border size
-
-//         for(var i = 1; i <= input; i++){
-//         for(var j = 1; j <= input; j++){
-
-//         $('#grid_container').append('<div class="grid_square"></div>');
-//         }
-//         $('#grid_container').append('<div class="new_row"></div>');
-//         }
-
-// $('.grid_square').css('width',square_size);
-// $('.grid_square').css('height',square_size);
-
-
-
-
-
-
-// else{
-// alert('Please pick a number between 1 and 12');
-// }
-// }
-// });
