1e8616241463899ae603838eb7001f4b2ca87b0a
Isaac Weinbach <isaacito@aol.com>
2015-01-26 15:06:14 -0500

    form

--- public/grid.js
+++ public/grid.js
@@ -1,16 +1,71 @@
-$(document).ready(function() {
+
+$(document).ready(function(){
 
 
-$( ".square" ).mouseenter(function() { 
-    $( this ).css("background", "red");
+$('#start').click(function(){
+   var input =  prompt('Type an integer between 1 and 12');
+   $('#grid_container').html(" ");
+
+if (input >= 1 && input <= 12 ){
+   
+   var square = $("#grid_container").width/input - 2
+
+        for (var i = 1; i <= input; i++){
+            for (var j = 1; j <= input; j ++){
+                 $('#grid_container').append('<div class="grid_square"></div>');
+            }
+        
+        $('#grid_container').append('<div class="new_row"></div>');
+        }
+        
+        $('.grid_square').css('width',square);
+        $('.grid_square').css('height',square);
+
+         $('.grid_square').mouseenter(function(){
+            $(this).addClass('grid_square_lit');
+        });
+
+
+   }
+   else{
+    alert("pick a # between 1 and 12")
+   }
 });
 
-$('.square').mouseleave(function(){
-        $(this).css("background", "black")
 
 
-} );
+});
 
 
 
-  });
+// var start = function(option){
+
+//     $('#grid_container').html("");
+
+//     var input = prompt('Type an integer between 1 and 12');
+
+//     if (input >= 1 && input <= 12){ 
+
+//     var square_size = $('#grid_container').width()/input - 2; // The -2 accounts for border size
+
+//         for(var i = 1; i <= input; i++){
+//         for(var j = 1; j <= input; j++){
+
+//         $('#grid_container').append('<div class="grid_square"></div>');
+//         }
+//         $('#grid_container').append('<div class="new_row"></div>');
+//         }
+
+// $('.grid_square').css('width',square_size);
+// $('.grid_square').css('height',square_size);
+
+
+
+
+
+
+// else{
+// alert('Please pick a number between 1 and 12');
+// }
+// }
+// });
